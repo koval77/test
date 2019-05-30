5406c95d54beb343e57b1431b8c3c36b3a4b2b64
koval77 <wojtek78@yahoo.com>
2019-05-30 07:53:09 +0100

    get nearest postcodes

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
 import json
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 import postcodes_io_api
-api  = postcodes_io_api.Api(debug_http=True)
-ps = api.get_postcode('SW112EF')
-
-
-print(ps)
 
 
 with open("stores.json","r") as stores:
@@ -21,15 +16,43 @@ print(type(obj[0]))
 
 #sr=sorted(obj,key=lambda i:i['name'])
 #print(sr)
-
+api = postcodes_io_api.Api(debug_http=True)
 app=Flask(__name__)
 
 @app.route("/")
 def result():
     sortedList = sorted(obj, key=lambda i: i['name'])
-    print(sortedList)
+    #api = postcodes_io_api.Api(debug_http=True)
+    for ell in sortedList:
+        #ell['lon']=3
+        #api = postcodes_io_api.Api(debug_http=True)
+        ell['postcode']=ell['postcode'].replace(" ","")
+        is_valid=api.is_postcode_valid(ell['postcode'])
+        if (is_valid==True):
+            ps = api.get_postcode(ell['postcode'])
+            print("\ntype of ps:{}".format(type(ps)))
+            langit=ps['result']['longitude']
+            latid=ps['result']['latitude']
+            print(langit)
+            ell['lon'] = api.get_postcode(ell['postcode'])['result']['longitude']
+            ell['lat'] = api.get_postcode(ell['postcode'])['result']['latitude']
+        else:
+            ell['lon'] = 'error'
+    #     #lan=api.get_postcode(ell['postcode'])['result']['longitude']
+    #     ell['lan']=api.get_postcode(ell['postcode'])['result']['longitude']
+    print("\nSSSSSSSSOOOOORRRTTEED \nLLLLIIIISSSTTT")
+    print("sortedList: {}".format(sortedList))
     #dict={'phy':50,'che':60,"math":34}
     return render_template("home.html",result=sortedList)
+
+@app.route('/storeslist',methods=['POST'])
+def storeslist():
+    if(request.method=='POST'):
+        storeslist=request.form
+        print("storeslist:{}".format(storeslist))
+        data = api.get_nearest_postcodes_for_postcode(postcode=storeslist['postcode'], radius=storeslist['distance'])
+        print("data:{}".format(data))
+        return render_template("storeslist.html",storeslist=data)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- main2.py
+++ main2.py
@@ -1,4 +1,7 @@
-import postcodes_io_api
-api  = postcodes_io_api.Api(debug_http=True)
-data = api.get_postcode('SW112EF')
-print(data)
+# import postcodes_io_api
+# api  = postcodes_io_api.Api(debug_http=True)
+# data = api.get_postcode('SW112EF')
+# print(data)
+ps="as34 4ed"
+psr=ps.replace(" ","")
+print(psr)
