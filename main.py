1f0e45634942a5cd9dab2662087805150422c118
koval77 <wojtek78@yahoo.com>
2019-06-03 04:31:52 +0100

    UI improvements, PEP8 check

--- main.py
+++ main.py
@@ -47,15 +47,15 @@ def add_coordinated_beta(list_of_places):
         List of dictionaries with added keys for longitude and latitude
     """
 
-    unpacked_list_dict_places_result=unpacking_api_data(list_of_places)
-    a=0
+    unpacked_list_dict_places_result = unpacking_api_data(list_of_places)
+    a = 0
     for element in list_of_places:
         if unpacked_list_dict_places_result[a] is not None:  # postcode validity check
-            element['lat']=unpacked_list_dict_places_result[a]['latitude']
-            element['lon']=unpacked_list_dict_places_result[a]['longitude']
-            a=a+1
+            element['lat'] = unpacked_list_dict_places_result[a]['latitude']
+            element['lon'] = unpacked_list_dict_places_result[a]['longitude']
+            a = a + 1
         else:
-            element['lon']='error'
+            element['lon'] = 'error'
             a = a + 1
     return list_of_places
 
@@ -72,10 +72,10 @@ def unpacking_api_data(list_of_data_dictionaries):
         Template which is translated to postcodes with stores.
     """
 
-    unpacked_list_dict_places=[]
-    postcodes_string_list=[i['postcode'] for i in list_of_data_dictionaries]
-    bulk_postcodes=api.get_bulk_postcodes(postcodes_string_list)
-    bulk_postcodes_unpacked=[i for i in bulk_postcodes['result']]
+    unpacked_list_dict_places = []
+    postcodes_string_list = [i['postcode'] for i in list_of_data_dictionaries]
+    bulk_postcodes = api.get_bulk_postcodes(postcodes_string_list)
+    bulk_postcodes_unpacked = [i for i in bulk_postcodes['result']]
     for temp in bulk_postcodes_unpacked:
         unpacked_list_dict_places.append(temp['result'])
     return unpacked_list_dict_places
@@ -97,15 +97,17 @@ def make_nearest_towns_list(my_postcode, desired_max_distance):
     list_of_dictionaries_of_nearest_towns:list
         Dictionaries have got keys for name, postcode and latitude.
     """
-    list_of_dictionaries_of_nearest_towns.clear() # Clearing allows to easily submit next queries after the first one
-    postcode_info=api.get_postcode(my_postcode.upper())
-    my_location_latitude=postcode_info['result']['latitude']
-    my_location_longitude=postcode_info['result']['longitude']
+    list_of_dictionaries_of_nearest_towns.clear()  # Clearing allows to easily submit next queries after the first one
+    postcode_info = api.get_postcode(my_postcode.upper())
+    my_location_latitude = postcode_info['result']['latitude']
+    my_location_longitude = postcode_info['result']['longitude']
     res = [i for i in sorted_json_list if i['lon'] != 'error']
     for element in res:
-        actual_distance=haversine((my_location_latitude, my_location_longitude), (element['lat'], element['lon']), unit=Unit.MILES)
+        actual_distance = haversine((my_location_latitude, my_location_longitude), (element['lat'], element['lon']),
+                                    unit=Unit.MILES)
         if int(actual_distance) < int(desired_max_distance):
-            list_of_dictionaries_of_nearest_towns.append({'name':element['name'],'postcode':element['postcode'],'latitude':element['lat']})
+            list_of_dictionaries_of_nearest_towns.append(
+                {'name': element['name'], 'postcode': element['postcode'], 'latitude': element['lat']})
     return list_of_dictionaries_of_nearest_towns
 
 
@@ -132,10 +134,10 @@ def preparing_stores_data(filename):
 
 app = Flask(__name__)  # Creating Flask object
 api = postcodes_io_api.Api(debug_http=False)
-list_of_dictionaries_of_nearest_towns=[]
-sorted_json_list=preparing_stores_data("stores.json") # creates list of dictionaries with name and postcode from json
+list_of_dictionaries_of_nearest_towns = []
+sorted_json_list = preparing_stores_data("stores.json")  # creates list of dictionaries with name and postcode from json
 # sortedList = add_coordinates_alpha(sortedList)
-sorted_json_list=add_coordinated_beta(sorted_json_list) # adding coordinates
+sorted_json_list = add_coordinated_beta(sorted_json_list)  # adding coordinates
 
 
 @app.route("/")
@@ -151,8 +153,8 @@ def storeslist():
     if request.method == 'POST':
         my_query = request.form
         if api.is_postcode_valid(my_query['postcode']) and int(my_query['distance']) > 0:  # validation check
-            nearest_towns=make_nearest_towns_list(my_query['postcode'],int(my_query['distance']))
-            sorted_by_lat_nearest_towns=sorted(nearest_towns,key=lambda i:i['latitude'],reverse=True)
+            nearest_towns = make_nearest_towns_list(my_query['postcode'], int(my_query['distance']))
+            sorted_by_lat_nearest_towns = sorted(nearest_towns, key=lambda i: i['latitude'], reverse=True)
             return render_template("storeslist.html", nearest_stores=sorted_by_lat_nearest_towns,
                                    how_many=len(sorted_by_lat_nearest_towns))
 
--- test_mine.py
+++ test_mine.py
@@ -1,7 +1,7 @@
 import unittest
 import main
 
-example=[{"name":"place","postcode":"HP201DH"}]
+example = [{"name": "place", "postcode": "HP201DH"}]
 
 
 class FlaskClientTestCase(unittest.TestCase):
@@ -26,30 +26,30 @@ class FlaskClientTestCase(unittest.TestCase):
         self.assertFalse('name given' in received)
 
     def test_filled_page_1(self):
-        response = self.client.post('/storeslist', data=example )
+        response = self.client.post('/storeslist', data=example)
         received = response.get_data(as_text=False)
         self.assertTrue(5 == received)
 
     def test_filled_page_2(self):
-        response = self.client.get('/storeslist', data = { 'name': 'Bob' } )
+        response = self.client.get('/storeslist', data={'name': 'Bob'})
         received = response.get_data(as_text=True)
         self.assertFalse('entering' in received)
 
     def test_add_coordinates(self):
-        self.assertEqual(len(example),len(main.add_coordinated_beta(example)))
+        self.assertEqual(len(example), len(main.add_coordinated_beta(example)))
 
     def test_number_of_nearest_towns(self):
-        self.a=main.make_nearest_towns_list("E58RA",5)
-        sorted_by_lat_nearest_towns=sorted(self.a,key=lambda i:i['latitude'],reverse=True)
-        self.assertEqual(len(sorted_by_lat_nearest_towns),3)
+        self.a = main.make_nearest_towns_list("E58RA", 5)
+        sorted_by_lat_nearest_towns = sorted(self.a, key=lambda i: i['latitude'], reverse=True)
+        self.assertEqual(len(sorted_by_lat_nearest_towns), 3)
 
     def test_home_status(self):
         response = self.client.get('/')
-        self.assertEqual(response.status_code,200)
+        self.assertEqual(response.status_code, 200)
 
     def test_stores_list_status(self):
         response = self.client.post('/storeslist')
-        self.assertEqual(response.status_code,200)
+        self.assertEqual(response.status_code, 200)
 
 
 suite = unittest.TestLoader().loadTestsFromTestCase(FlaskClientTestCase)
